3843d89a7a1b4ba73b607a1a29e8b73829aa96a3
Valdo Mpinga <valdompinga2@gmail.com>
2024-11-28 16:18:57 +0000

    first commit

---
+++ src/main/java/com/example/witchallengevaldompinga/WitChallengeValdoMpingaApplication.java
@@ -0,0 +1,13 @@
+package com.example.witchallengevaldompinga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitChallengeValdoMpingaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WitChallengeValdoMpingaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/witchallengevaldompinga/WitChallengeValdoMpingaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.witchallengevaldompinga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitChallengeValdoMpingaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
